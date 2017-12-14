1cdcb9d69d6279aa7619f9f9c33a6d5f75880bc3
Casey Kelly <casey.kelly@colorado.edu>
2017-12-13 18:34:54 -0700

    Add integration tests for weather chart and search functionality

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,5 +1,21 @@
-describe('Get a forecast for a city', function() {
-    it('Visits the weather chart site', function() {
+describe('Get a forecast for a city', () => {
+    it('Visits the weather chart site', () => {
         cy.visit('http://localhost:8080')
+    })
+
+    it('Finds the search bar', () => {
+        cy.get('input').should('be.visible')
+    })
+
+    it('Displays a weather chart when user searches for a city', () => {
+        cy.get('input').type('Denver')
+        cy.contains('Submit').click()
+        cy.get('table tbody tr:first').should('have.attr', 'data-reactid', '.0.1.1.$Denver')
+    })
+
+    it('Displays a second weather chart above the first when user searches for a another city', () => {
+        cy.get('input').type('Boulder')
+        cy.contains('Submit').click()
+        cy.get('table tbody tr:first').should('have.attr', 'data-reactid', '.0.1.1.$Boulder')
     })
 })
